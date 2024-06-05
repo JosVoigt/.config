ea9d1b4a780c1681edc5872e304d1e5806d55d1d
Joshua Voigt <joshua.voigt@tum.de>
2024-06-05 13:48:57 +0200

    updated nvim

--- nvim/lua/dayoshi/settings.lua
+++ nvim/lua/dayoshi/settings.lua
@@ -1,3 +1,20 @@
 vim.opt.nu = true
 vim.opt.scrolloff = 15
 vim.opt.wrap = false
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.inccommand = "split"
+
+vim.opt.cursorline = true
+
+vim.opt.timeoutlen = 200
+
+vim.opt.undofile = true
+
+vim.opt.showmode = false
+
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
