62d8b513537225c4113b35f86f867eb71faa5ac0
Joshua Voigt <joshua.voigt@tum.de>
2024-06-12 16:28:08 +0200

    telescope upgrades

--- nvim/lua/dayoshi/plugins/telescope.lua
+++ nvim/lua/dayoshi/plugins/telescope.lua
@@ -3,7 +3,23 @@ return {
 	tag = "0.1.8",
 	dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope-file-browser.nvim" },
 	config = function()
-		require("telescope").setup()
+		require("telescope").setup({
+			defaults = {
+				prompt_prefix = "🔎 ",
+				selection_caret = "->",
+				entry_prefix = "- ",
+				file_ignore_patterns = { "%.o%", "^%build%/", "^%debug%/" },
+				theme = "dropdown",
+				sorting_strategy = "ascending",
+				layout_config = {
+					horizontal = {
+						height = 0.999,
+						width = 0.999,
+						prompt_position = "top",
+					},
+				},
+			},
+		})
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>o", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
